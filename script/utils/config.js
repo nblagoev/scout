fb9f817f908caaf909831614fcf491a6a2d14f4d
Nikolay Blagoev <nikolay.blagoev@gmail.com>
2015-04-20 21:16:56 +0300

    :green_heart: Setup a SCOUT_HOME env variable for the grunt tasks

--- script/utils/config.js
+++ script/utils/config.js
@@ -3,6 +3,8 @@ var pkg = require('../../package.json');
 
 if (process.platform === 'win32') {
   process.env.ATOM_HOME = path.join(process.env.USERPROFILE, '.' + pkg.name);
+  process.env.SCOUT_HOME = path.join(process.env.USERPROFILE, '.scout');
 } else {
   process.env.ATOM_HOME = path.join(process.env.HOME, '.' + pkg.name);
+  process.env.SCOUT_HOME = path.join(process.env.HOME, '.scout');
 }
